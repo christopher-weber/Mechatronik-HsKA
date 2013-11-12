b9055c0ae672f9ae09b1738bf9b9ac8452d4049d
christopher-weber <christopher-weber@web.de>
2013-11-12 18:22:58 +0100

    sdfsfd

--- js/campus_map.js
+++ js/campus_map.js
@@ -57,8 +57,9 @@
 		
 			// Die aktuelle Position konnte nicht ermittelt werden.
 			function onError(error) {
-				no_pos_info();													// Karte wird ohne Standortinformationen erstellt
 				$("#nopos").popup("open", {positionTo: "window"});				// Popup mit Fehlermeldung wird angezeigt
+				
+								no_pos_info();													// Karte wird ohne Standortinformationen erstellt
 			}
   
   
